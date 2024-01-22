2fb5198c7efb6be1e89acf447318096efe70e94c
Henrique Kubinha <rickubinhetz@gmail.com>
2024-01-22 17:00:55 -0300

    Small improvements

--- prototype.py
+++ prototype.py
@@ -1,7 +1,18 @@
 # This code is used for eventual tests.
 
-from datetime import datetime
+import tkinter as tk
+from tkinter import filedialog as fd
+import glob
 
-print(datetime.today().strftime('%d-%m'))
+root = tk.Tk()
+root.withdraw()
 
-# ------
+filepath = fd.askdirectory(initialdir="C:\\Users\\CS317813\\Desktop\\Dados TBG - Amostragem",
+                           title="Escolha seu Arquivo!").replace("/", "\\")
+
+files = glob.glob(filepath + "*.csv")
+print(files)
+
+print(type(filepath))
+
+
